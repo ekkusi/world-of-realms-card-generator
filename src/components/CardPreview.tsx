e9a45ddd8f3aa57128b0903fd0c2d890df97de13
Ekku Sipilä <ekku.eki@gmail.com>
2024-08-27 14:32:29 +0300

    Small UI fixes

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -10,7 +10,7 @@ export type CardPreviewProps = TailWindProps & Partial<Card> & {
 export default function CardPreview(props: CardPreviewProps) {
   const { className, ...card } = props;
   return (
-    <div className={cn("relative overflow-hidden rounded-lg", className)}>
+    <div className={cn("relative overflow-hidden rounded-lg max-w-[400px]", className)}>
       {card.bgImgUrl ?
         <>
           <div className="relative z-10">
@@ -25,17 +25,17 @@ export default function CardPreview(props: CardPreviewProps) {
           <div className="absolute z-20 top-[4%] left-[6%] max-w-[20%] text-white">
             <Image src="/cards/damage_icon.png" width={80} height={80} alt="Damage" />
             {!Number.isNaN(card.damage) && <span className="text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{card.damage}</span>}
-            <span className="text-md absolute bottom-[14%] w-full text-center text-white">DAMAGE</span>
+            <span className="text-sm absolute bottom-[14%] w-full text-center text-white">DAMAGE</span>
           </div>
           <div className="absolute z-20 top-[4%] right-[6%] max-w-[20%] text-white">
             <Image src="/cards/health_icon.png" width={80} height={80} alt="Health" />
             {!Number.isNaN(card.health) && <span className="text-3xl absolute top-1/2 left-1/2 -translate-x-[65%] -translate-y-1/2">{card.health}</span>}
-            <span className="text-md absolute bottom-[14%] w-full text-center">HEALTH</span>
+            <span className="text-sm absolute bottom-[14%] w-full text-center">HEALTH</span>
           </div>
           <div className="absolute z-20 bottom-[30%] left-[6%] max-w-[20%] text-white">
             <Image src="/cards/influence_icon.png" width={80} height={80} alt="Influence" />
             {!Number.isNaN(card.influence) && <span className="text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{card.influence}</span>}
-            <span className="text-xs absolute bottom-[10%] w-full text-center">INFLUENCE</span>
+            <span className="text-sm absolute bottom-[10%] w-full text-center">INFLUENCE</span>
           </div>
           <div className="absolute z-20 bottom-[30%] right-[6%] max-w-[20%] text-white">
             <Image src={card.cardType === CardType.ENTITY ? "/cards/summon_icon.png" : "/cards/energy_icon.png"} width={80} height={80} alt="Cost" />
